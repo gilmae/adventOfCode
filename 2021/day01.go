569807fe9374911a4c4e590d912ba4ceb7e22870
gilmae <avocadia@fastmail.fm>
2021-12-01 16:30:46 +1100

    Cleansup solution

--- 2021/day01.go
+++ 2021/day01.go
@@ -9,6 +9,7 @@ import (
 )
 
 var inputFile = flag.String("inputFile", "inputs/day01.input", "Relative file path to use as input.")
+var part = flag.String("part", "a", "Which part to solve")
 
 func main() {
 	flag.Parse()
@@ -19,6 +20,33 @@ func main() {
 	contents := string(bytes)
 	lines := strings.Split(contents, "\n")
 
+	switch *part {
+	case "a":
+		fmt.Println(partA(lines))
+	case "b":
+		fmt.Println(partB(lines))
+	default:
+		fmt.Printf("Unknown part: %s\n", *part)
+	}
+
+}
+
+func partA(lines []string) int {
+	previous := 0
+
+	count := -1
+	for i := range lines {
+		depth, _ := strconv.Atoi(lines[i])
+
+		if depth > previous {
+			count++
+		}
+		previous = depth
+	}
+	return count
+}
+
+func partB(lines []string) int {
 	previous := 0
 
 	count := -1
@@ -33,6 +61,5 @@ func main() {
 		}
 		previous = depth
 	}
-
-	fmt.Println(count)
+	return count
 }
