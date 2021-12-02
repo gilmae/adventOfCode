f62a18f147226a08628b9ce77a49e89714be3fc8
gilmae <avocadia@fastmail.fm>
2021-12-03 08:20:11 +1100

    Solves 2015 day 20 part 2

--- 2015/day20.go
+++ 2015/day20.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	goal := 33100000 / 10
+	goal := 33100000 / 11
 	house := 1
 	for sumDivisors(house) < goal {
 		house++
@@ -16,7 +16,14 @@ func sumDivisors(house int) int {
 	sum := 0
 	for i := 1; i*i < house; i++ {
 		if house%i == 0 {
-			sum += i + house/i
+			j := house / i
+			if j <= 50 {
+				sum += i
+			}
+			if i <= 50 {
+				sum += j
+			}
+
 		}
 	}
 	return sum
