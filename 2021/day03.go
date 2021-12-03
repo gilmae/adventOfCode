a93968f7c669151c8b8854d441ed3c6093872eda
gilmae <avocadia@fastmail.fm>
2021-12-03 16:33:14 +1100

    Solves 2021 day 3 part b
    Ugh...ugly

--- 2021/day03.go
+++ 2021/day03.go
@@ -8,7 +8,6 @@ import (
 )
 
 var inputFile = flag.String("inputFile", "inputs/day03.input", "Relative file path to use as input.")
-var part = flag.String("part", "a", "Which part to solve")
 
 func main() {
 	flag.Parse()
@@ -35,4 +34,63 @@ func main() {
 	}
 
 	fmt.Println(epsilon * gamma)
+
+	oxygens := make([]string, len(lines))
+	copy(oxygens, lines)
+
+	for i := range lines[0] {
+		oxygens = findOxygenReading(oxygens, i)
+		if len(oxygens) == 1 {
+			break
+		}
+	}
+
+	carbons := make([]string, len(lines))
+	copy(carbons, lines)
+	for i := range lines[0] {
+		carbons = findC02Reading(carbons, i)
+		if len(carbons) == 1 {
+			break
+		}
+	}
+	fmt.Println(fromBinaryString(oxygens[0]) * fromBinaryString(carbons[0]))
+}
+
+func findOxygenReading(readings []string, pos int) []string {
+	splits := map[byte][]string{'0': make([]string, 0), '1': make([]string, 0)}
+
+	for _, l := range readings {
+		splits[l[pos]] = append(splits[l[pos]], l)
+	}
+	if len(splits['0']) > len(splits['1']) {
+		return splits['0']
+	} else {
+		return splits['1']
+	}
+}
+
+func findC02Reading(readings []string, pos int) []string {
+	splits := map[byte][]string{'0': make([]string, 0), '1': make([]string, 0)}
+
+	for _, l := range readings {
+		splits[l[pos]] = append(splits[l[pos]], l)
+	}
+	if len(splits['0']) <= len(splits['1']) {
+		return splits['0']
+	} else {
+		return splits['1']
+	}
+}
+
+func fromBinaryString(str string) int {
+	result := 0
+	slen := len(str) - 1
+	for i, ch := range str {
+		pos := slen - i
+		if ch == '1' {
+			result += 1 << pos
+		}
+	}
+
+	return result
 }
