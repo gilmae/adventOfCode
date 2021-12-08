286fe89ae62628a7a64d9ca90878ef8a57c51be0
gilmae <avocadia@fastmail.fm>
2021-12-08 12:38:42 +1100

    Solves 2019 day 4 part b

--- 2019/day04.go
+++ 2019/day04.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"strconv"
 )
 
+var part = flag.String("part", "a", "Which part")
+
 func main() {
+	flag.Parse()
 	passwd := 382344
 	validCount := 0
 	for passwd <= 843167 {
@@ -20,17 +24,37 @@ func main() {
 }
 
 func isValid(passwd int) bool {
-	passwdS := strconv.Itoa(passwd + 1)
+	digits := strconv.Itoa(passwd + 1)
+	var previous rune
+	var double bool
+	var exactDouble bool
+	run := 1
 
-	for i := 0; i < len(passwdS)-1; i++ {
-		if passwdS[i+1] < passwdS[i] {
+	for _, r := range digits {
+		if previous == r {
+			double = true
+			run++
+		} else {
+			if run == 2 {
+				exactDouble = true
+			}
+			run = 1
+		}
+		if previous > r {
 			return false
 		}
+
+		previous = r
 	}
-	for i := 0; i < len(passwdS)-1; i++ {
-		if passwdS[i+1] == passwdS[i] {
-			return true
-		}
+
+	if run == 2 {
+		exactDouble = true
 	}
-	return false
+
+	if *part == "b" {
+		return exactDouble
+	} else {
+		return double
+	}
+
 }
