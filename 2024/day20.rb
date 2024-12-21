4a777f53f3f28f9c5275e1d97570c10a6cebd45d
gilmae <avocadia@fastmail.fm>
2024-12-21 14:38:01 +1100

    Performance improvements for day 20

--- 2024/day20.rb
+++ 2024/day20.rb
@@ -8,7 +8,8 @@ data.each_with_index { |line, y|
     start = [x,y] if c == "S"
   }
 }
-
+H = data.length
+W = data[0].length
 steps_at_point = {start=>0}
 
 work=[start]
@@ -30,13 +31,23 @@ loop {
 
 part_a_cheats = 0
 part_b_cheats = 0
-steps_at_point.keys.combination(2).each {|p1,p2|
-  cheat = (steps_at_point[p2]-steps_at_point[p1])
-  m = (p1[0]-p2[0]).abs + (p1[1]-p2[1]).abs
-  next if (cheat-m)<100
-  
-  part_a_cheats+=1 if m == 2
-  part_b_cheats+=1 if m <= 20
+
+steps_at_point.keys.each {|p1|
+  (-20..20).each {|x|
+    (-20..20).each {|y|
+      next if x==0 && y==0
+      m = (x).abs + (y).abs
+      next if m > 20
+      p2 = [p1[0]+x,p1[1]+y]
+      next unless steps_at_point.has_key? p2
+
+      cheat = (steps_at_point[p2]-steps_at_point[p1])
+      next if (cheat-m)<100
+
+      part_a_cheats+=1 if m == 2
+      part_b_cheats+=1 if m <= 20
+    }
+  }
 }
 pp part_a_cheats
 pp part_b_cheats
